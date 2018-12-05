3aabb28dfa7dba565979b665decd070b0015c854
Unknown <wfaengrit@gmail.com>
2018-12-05 22:08:10 +0700

    create into function

--- circle_detection.py
+++ circle_detection.py
@@ -2,6 +2,25 @@
 import numpy as np
 import cv2
 
+def detect_circle(frame):
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray_blur = cv2.GaussianBlur(gray, (15, 15), 0)
+    #thresh = cv2.adaptiveThreshold(gray_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV, 11, 1)
+    #kernel = np.ones((3, 3), np.uint8)
+    #closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=4)
+    #output = gray_blur
+    rows = gray.shape[0]
+    circles = cv2.HoughCircles(gray_blur,cv2.HOUGH_GRADIENT,1, rows / 9,
+                               param1=100, param2=30,
+                               minRadius=10, maxRadius=100)
+    if circles is not None:
+        print("circle")
+        circles = np.round(circles[0, :]).astype("int")
+        for (x, y, r) in circles:
+            cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
+            cv2.rectangle(frame, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+    return frame
+'''
 cap = cv2.VideoCapture(0)
 while(True):
     #read image
@@ -32,3 +51,4 @@ while(True):
 
 cap.release()
 cv2.destroyAllWindows()
+'''
